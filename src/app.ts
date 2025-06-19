9f8ba0d129a526a9a0eb1f2c53a659289b857bbb
imtiazsakib15 <sakib.imtiaz15@gmail.com>
2025-06-19 12:21:44 +0600

    create admin api created

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,17 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
+import { UserRoutes } from "./modules/user/user.route";
 
 const app: Application = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.get("/", (req: Request, res: Response) => {
   res.send({ message: "Server is running..." });
 });
+
+app.use("/api/v1/users", UserRoutes);
 
 export default app;
---
+++ src/modules/user/user.service.ts
@@ -0,0 +1,25 @@
+import { PrismaClient, UserRole } from "../../../generated/prisma";
+import bcrypt from "bcrypt";
+const prisma = new PrismaClient();
+
+const createAdmin = async (data: any) => {
+  const result = await prisma.$transaction(async (tx) => {
+    const userInfo = {
+      email: data.admin.email,
+      password: await bcrypt.hash(data.password, 12),
+      role: UserRole.ADMIN,
+    };
+    await tx.user.create({
+      data: userInfo,
+    });
+    const createdAdmin = await tx.admin.create({
+      data: data.admin,
+    });
+
+    return createdAdmin;
+  });
+
+  return result;
+};
+
+export const UserService = { createAdmin };
