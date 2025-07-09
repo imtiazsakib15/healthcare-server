844857ffbca2bacdd76f3e4ae618ed0f7d56c502
imtiazsakib15 <sakib.imtiaz15@gmail.com>
2025-07-09 11:54:31 +0600

    Specialty and DoctotSpecialty model created

---
+++ prisma/migrations/20250709055414_specialty_and_doctor_specialty_model_added/migration.sql
@@ -0,0 +1,23 @@
+-- CreateTable
+CREATE TABLE "specialties" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "icon" TEXT NOT NULL,
+
+    CONSTRAINT "specialties_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "doctor_specialties" (
+    "doctorId" TEXT NOT NULL,
+    "specialtiesId" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "doctor_specialties_doctorId_specialtiesId_key" ON "doctor_specialties"("doctorId", "specialtiesId");
+
+-- AddForeignKey
+ALTER TABLE "doctor_specialties" ADD CONSTRAINT "doctor_specialties_doctorId_fkey" FOREIGN KEY ("doctorId") REFERENCES "doctors"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "doctor_specialties" ADD CONSTRAINT "doctor_specialties_specialtiesId_fkey" FOREIGN KEY ("specialtiesId") REFERENCES "specialties"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
